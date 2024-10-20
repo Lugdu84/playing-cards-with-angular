3dde51e22d74491fa09d7e438603f2656bab86fb
David Grammatico <david.lugdunum@gmail.com>
2024-10-20 20:40:55 +0200

    first commit

---
+++ src/app/models/monster.model.ts
@@ -0,0 +1,9 @@
+export class Monster {
+  name = 'My Monster';
+  hp = 40;
+  figureCaption = 'N°001 Monster';
+  attackName = 'Geo Impact';
+  attackStrength = 60;
+  attackDescription =
+    'This is a long description of a monster attack. Probably something to do with electricity...';
+}
